81cc3b6e8d6629e781be7aa9af3e70b861369923
shamylkashif <144819710+shamylkashif@users.noreply.github.com>
2024-05-17 22:43:21 +0500

    commit

--- lib/home-screen.dart
+++ lib/home-screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:thebookstore/commons/colors.dart';
+import 'package:thebookstore/screens/bottom-bar/MyAds.dart';
+import 'package:thebookstore/screens/bottom-bar/account.dart';
+import 'package:thebookstore/screens/bottom-bar/chats.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +12,61 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    switch (index) {
+      case 0:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeScreen()));
+        break;
+      case 1:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => Chat()));
+        break;
+      case 2:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => MyAds()));
+        break;
+      case 3:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => Account()));
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: backGround,
+
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'My ADS',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+
+        onTap: _onItemTapped,
+      ),
+
+    );
   }
 }
---
+++ lib/screens/bottom-bar/MyAds.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAds extends StatefulWidget {
+  const MyAds({super.key});
+
+  @override
+  State<MyAds> createState() => _MyAdsState();
+}
+
+class _MyAdsState extends State<MyAds> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
